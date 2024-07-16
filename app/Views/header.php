414f87662b54c555ae9aa9b455fe69066dfcdff8
Santiago24x <santiagomarquez.it@gmail.com>
2024-07-16 11:50:59 -0500

    Actualizacion del header para poder administrar

--- app/Views/header.php
+++ app/Views/header.php
@@ -67,6 +67,18 @@
                             </a>
 
 
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#subAdministracion" aria-expanded="false" aria-controls="subAdministracion">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-screwdriver-wrench"></i></div>
+                                ADMINISTRACION
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="subAdministracion" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="<?php echo base_url() ?>configuracion">Configuracion</a>
+                                </nav>
+                            </div>
+
+
 
                         </div>
                     </div>
